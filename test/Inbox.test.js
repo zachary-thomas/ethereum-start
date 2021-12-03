630857c2d10335a48db4fccb75771544b831a32a
Zac <thomazac@mail.gvsu.edu>
2021-12-03 16:16:29 -0600

    add Web3 instance for tests

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,3 +3,7 @@ const ganache = require('ganache-cli');
 
 // Constructor function, capitilized
 const Web3 = require('web3');
+
+// Instance of web3 to connect to test network
+// through the provider (provider depends on network)
+const web3 = new Web3(ganache.provider());

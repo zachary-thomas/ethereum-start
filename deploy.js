c4ded232555b963dbf4e01765e61fe869511c3f3
Zac <thomazac@mail.gvsu.edu>
2021-12-28 17:41:26 -0600

    write deploy script to publish to rinkeby network

--- deploy.js
+++ deploy.js
@@ -1,8 +1,7 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-const web3 = require('web3');
+const Web3 = require('web3');
 const {interface, bytecode} = require ('./compile');
 const constants = require('./constants');
-const { default: Web3 } = require('web3');
 
 const provider = new HDWalletProvider(
     constants.MNEMONIC,
@@ -10,3 +9,19 @@ const provider = new HDWalletProvider(
 );
 
 const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account: ', accounts[0])
+
+    const resultContract = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data: bytecode, arguments: ['Hi there!'] })
+        .send({ gas: '1000000', from: accounts[0] });
+
+    console.log('Contract deployed to', resultContract.options.address);
+    provider.engine.stop();
+};
+
+// Helper to make async
+deploy();
